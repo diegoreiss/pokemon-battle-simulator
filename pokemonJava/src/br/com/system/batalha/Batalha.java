527b94c2bd98ee1bea92011148a614865685c71a
diegoreiss <diegoportal.reis@outlook.com>
2022-11-26 14:09:28 -0300

    classe batalha com Constructor e getters e setters

--- pokemonJava/src/br/com/system/batalha/Batalha.java
+++ pokemonJava/src/br/com/system/batalha/Batalha.java
@@ -1,4 +1,21 @@
 package br.com.system.batalha;
 
+import br.com.system.treinador.LiderDeGinasio;
+import br.com.system.treinador.TreinadorComum;
+
 public class Batalha {
+
+    private TipoBatalha tipoBatalha;
+
+    public Batalha(TipoBatalha tipoBatalha) {
+        this.tipoBatalha = tipoBatalha;
+    }
+
+    public TipoBatalha getTipoBatalha() {
+        return tipoBatalha;
+    }
+
+    public void setTipoBatalha(TipoBatalha tipoBatalha) {
+        this.tipoBatalha = tipoBatalha;
+    }
 }
