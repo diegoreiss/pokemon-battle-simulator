447949936ce913ad73b72717473b20c14dc07c7f
diegoreiss <diegoportal.reis@outlook.com>
2022-11-26 13:35:45 -0300

    adicionando o atributo List<insignia> na classe TreinadorComum

--- pokemonJava/src/br/com/system/treinador/TreinadorComum.java
+++ pokemonJava/src/br/com/system/treinador/TreinadorComum.java
@@ -4,14 +4,29 @@ import br.com.system.mochila.Mochila;
 import br.com.system.pokemon.Genero;
 import br.com.system.pokemon.Pokemon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreinadorComum extends Treinador {
+
+    private List<Insignia> insignias;
 
     public TreinadorComum(String nome, int idade, Genero genero, Mochila mochila) {
         super(nome, idade, genero, mochila);
+        this.insignias = new ArrayList<>()
     }
 
     public TreinadorComum(String nome, int idade, Genero genero, Mochila mochila, Pokemon[] pokemons) {
         super(nome, idade, genero, mochila, pokemons);
+        this.insignias = new ArrayList<>();
+    }
+
+    public List<Insignia> getInsignias() {
+        return insignias;
+    }
+
+    public void setInsignias(List<Insignia> insignias) {
+        this.insignias = insignias;
     }
 
     @Override
