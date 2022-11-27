82d3915ec19b988725bd14ae51ba9414e1e19394
diegoreiss <diegoportal.reis@outlook.com>
2022-11-26 23:03:49 -0300

    metodo fugirDaBatalha na Classe Batalha

--- pokemonJava/src/br/com/system/batalha/Batalha.java
+++ pokemonJava/src/br/com/system/batalha/Batalha.java
@@ -1,7 +1,6 @@
 package br.com.system.batalha;
 
-import br.com.system.treinador.LiderDeGinasio;
-import br.com.system.treinador.TreinadorComum;
+import java.util.Random;
 
 public class Batalha {
 
@@ -18,4 +17,18 @@ public class Batalha {
     public void setTipoBatalha(TipoBatalha tipoBatalha) {
         this.tipoBatalha = tipoBatalha;
     }
+
+    public boolean fugirDaBatalha() throws Exception {
+
+        if (this.tipoBatalha.equals(TipoBatalha.BATALHA_POKEMON_COM_TREINADOR)) {
+            throw new Exception("Não pode fugir de uma batalha com treinador!");
+        }
+
+        boolean[] booleans = {true, false};
+
+        int randomInt = new Random().nextInt(booleans.length);
+
+        return booleans[randomInt];
+    }
+
 }
