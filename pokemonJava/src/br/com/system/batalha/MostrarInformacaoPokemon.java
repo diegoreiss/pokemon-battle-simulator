728fc3641a4d479bede6f8f8b4b653756601d366
diegoreiss <diegoportal.reis@outlook.com>
2022-12-04 13:39:40 -0300

    Classe dependencia

---
+++ pokemonJava/src/br/com/system/batalha/MostrarInformacaoPokemon.java
@@ -0,0 +1,19 @@
+package br.com.system.batalha;
+
+import br.com.system.pokemon.Pokemon;
+
+public class MostrarInformacaoPokemon {
+    static void mostrarInformacoesPokemons(Pokemon pokemonPlayer, Pokemon pokemonAdversario) {
+        System.out.printf(
+                "%n" +
+                        "%s [%c] : %s%n" +
+                        "   hp: %d/%d%n" +
+                        "%s [%c] : %s%n" +
+                        "   hp: %d/%d%n",
+                pokemonAdversario.getNome(), pokemonAdversario.getSIMBOLO_GENERO(), pokemonAdversario.getTipos(),
+                pokemonAdversario.getHpAtual(), pokemonAdversario.getHP_TOTAL(),
+                pokemonPlayer.getNome(), pokemonPlayer.getSIMBOLO_GENERO(), pokemonPlayer.getTipos(),
+                pokemonPlayer.getHpAtual(), pokemonPlayer.getHP_TOTAL()
+        );
+    }
+}
