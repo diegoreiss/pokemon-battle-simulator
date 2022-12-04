7d45a6be7da1bd0e17458abc49ca44415f5c1eff
diegoreiss <diegoportal.reis@outlook.com>
2022-12-04 13:41:40 -0300

    ajustando metodo mostrarInformacoesPokemon

--- pokemonJava/src/br/com/system/batalha/BatalhaVsLider.java
+++ pokemonJava/src/br/com/system/batalha/BatalhaVsLider.java
@@ -93,7 +93,7 @@ public class BatalhaVsLider implements Batalha {
             }
 
             while (true) {
-                Batalha.mostrarInformacoesPokemons(this.pokemonPlayerEscolhido, this.pokemonAdversarioEscolhido);
+                MostrarInformacaoPokemon.mostrarInformacoesPokemons(this.pokemonPlayerEscolhido, this.pokemonAdversarioEscolhido);
 
                 try {
                     turnoPlayer();
