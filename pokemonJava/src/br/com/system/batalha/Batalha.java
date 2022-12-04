67d2624501210928f0e3bb830b943f8be411b05d
diegoreiss <diegoportal.reis@outlook.com>
2022-12-04 13:40:56 -0300

    removendo metodo Static mostrarInformacoesPokemons

--- pokemonJava/src/br/com/system/batalha/Batalha.java
+++ pokemonJava/src/br/com/system/batalha/Batalha.java
@@ -19,20 +19,6 @@ public interface Batalha {
     void verificarHpPokemons(Pokemon pokemonPlayer, Pokemon pokemonAdversario);
     Object verificarVencedor();
 
-    static void mostrarInformacoesPokemons(Pokemon pokemonPlayer, Pokemon pokemonAdversario) {
-        System.out.printf(
-                "%n" +
-                        "%s [%c] : %s%n" +
-                        "   hp: %d/%d%n" +
-                        "%s [%c] : %s%n" +
-                        "   hp: %d/%d%n",
-                pokemonAdversario.getNome(), pokemonAdversario.getSIMBOLO_GENERO(), pokemonAdversario.getTipos(),
-                    pokemonAdversario.getHpAtual(), pokemonAdversario.getHP_TOTAL(),
-                pokemonPlayer.getNome(), pokemonPlayer.getSIMBOLO_GENERO(), pokemonPlayer.getTipos(),
-                    pokemonPlayer.getHpAtual(), pokemonPlayer.getHP_TOTAL()
-        );
-    }
-
     static void verificarHp(Pokemon pokemonQueRecebeuAtaque) throws PokemonAbatidoException {
         if (pokemonQueRecebeuAtaque.getHpAtual() <= 0) {
             throw new PokemonAbatidoException("Pokemon " + pokemonQueRecebeuAtaque.getNome() + " foi abatido!");
